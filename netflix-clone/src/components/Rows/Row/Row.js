8b38b3089ef96e44036769ab04a0728eccd60f00
habrev <samuelgashu11@gmail.com>
2024-08-27 18:16:16 +0300

    row file creation

---
+++ netflix-clone/src/components/Rows/Row/Row.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const row = () => {
+  return (
+    <div>
+      row
+    </div>
+  )
+}
+
+export default row
---
+++ netflix-clone/src/components/Rows/RowList/RowList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Row from '../Row/Row'
+
+const RowList = () => {
+  return (
+    <>
+      <Row />
+      <Row />
+      <Row />
+      <Row />
+    </>
+  )
+}
+
+export default RowList
